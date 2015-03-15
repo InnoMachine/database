f33a46e91e5aae08660a0c872833cf10ce90b0bd
InnoMachine <jx13@software.nju.edu.cn>
2015-03-15 19:39:00 +0800

    update

--- serve/getUserSchedule.php
+++ serve/getUserSchedule.php
@@ -1,12 +1,12 @@
 <?php
 require_once('schedule.php');
 require_once('userInf.php');
-$uid=$_POST['uid'];
-$cid=$_POST['cid'];
+$uid=$_POST['user_id'];
+$cid=$_POST['contact_id'];
 $token=$_POST['token'];
-#echo ["status":"1","schedule":{"activity1":{"content":"do something","id":"0001"}},"size":...];
 
-echo ["1",[[],[],[]],[[],[],[]],[],[]];
+
+#echo ["status":"1","schedule":{"activity1":{"content":"do something","id":"0001"}},"size":...];
 
 $name=getId(name);
 echo getSchedule($name,$stime,$length);
--- serve/match.php
+++ serve/match.php
@@ -3,11 +3,15 @@ require_once('schedule.php');
 require_once('userInf.php');
 require_once('user.php');
 
-$id=$_POST['id'];
-$idarray=$_POST['idarray'];
-$btime=$_POST['begintime'];
-$etime=$_POST['endtime'];
+$id=$_POST['user_id'];
+$idarray=$_POST['user_id_array'];
+$btime=$_POST['begin_time'];
+$etime=$_POST['end_time'];
 $token=$_POST['token'];
+
+#array() activityArray;
+
+
 
 #echo [“schedule”:{“activity1”:{“content”:”do something”,”id”:”0001”}··········},”size”:”49”,”begin_time”:”2014- 3-4 8:00”]
 
